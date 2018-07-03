a07801b99ced10879f194d708dacab1487db5814
Leandro-Avila <31443383+Leandro-Avila@users.noreply.github.com>
2018-07-03 19:07:12 -0300

    Delete decompress.py

--- Metamorfose O jogo/decompress.py
+++
@@ -1,9 +0,0 @@
-import zlib
-
-
-def decompress(file):
-    out_file = open (file + '.txt', "rb")
-    decompressed = zlib.decompress(out_file.read())
-
-    dec_file = open (file + '.jpg', "wb")
-    dec_file.write(decompressed)
